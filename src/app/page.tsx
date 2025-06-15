9c4d7e0748c0bc583b3e325c11ddc70fa7e72540
Camillebzd <bcamille99@gmail.com>
2025-06-15 10:22:00 +0100

    [FIX] using websocket to listen for every events and match with user address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,119 +1,130 @@
 "use client"
 
 import { useActiveAccount, useWalletBalance } from "thirdweb/react";
-import { useEffect, useState } from "react";
-import { Button, Flex, Heading, Input, Text, useToast } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import { Button, Flex, Heading, Input, Stat, StatGroup, StatLabel, StatNumber, Text, useToast } from "@chakra-ui/react";
 import { ethers } from "ethers";
 import styles from './page.module.css'
-import { createCoinflip, flip } from "@/utils/coinflip";
+import { flip } from "@/utils/coinflip";
 import { monadTestnet } from "@/lib/chain";
 import { client } from "@/lib/client";
+import coinflipABI from '@/abi/Coinflip.json';
 
 const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_COINFLIP_CONTRACT_ADDRESS || "";
-const RPC_PROVIDER = process.env.NEXT_PUBLIC_RPC_PROVIDER || "";
+const WEBSOCKET_PROVIDER = `wss://monad-testnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || ''}`;
 
+/// The page is only using the user address to listen for events, we can't use the
+/// sequence number as sometimes the events are received out of order
 export default function Home() {
   const [coinFace, setCoinFace] = useState<'HEADS' | 'TAILS'>('HEADS');
   const [amountToBet, setAmountToBet] = useState('')
   const [isLoading, setIsLoading] = useState(false);
   const [sequenceNumber, setSequenceNumber] = useState(0);
+  const sequenceNumberRef = useRef(sequenceNumber);
   const acc = useActiveAccount();
   const toast = useToast();
-  const { data: contractBalance, isLoading: isContractBalanceLoading } = useWalletBalance({
+  const { data: contractBalance, isLoading: isContractBalanceLoading, refetch: refetchContractBalance } = useWalletBalance({
     chain: monadTestnet,
     address: CONTRACT_ADDRESS,
     client: client,
   });
+  const { data: userBalance, isLoading: isUserBalanceLoading, refetch: refetchUserBalance } = useWalletBalance({
+    chain: monadTestnet,
+    address: acc?.address,
+    client: client,
+  });
+
+  // Keep the ref updated with the latest sequenceNumber
+  useEffect(() => {
+    sequenceNumberRef.current = sequenceNumber;
+  }, [sequenceNumber]);
+
 
   useEffect(() => {
-    if (!acc || sequenceNumber === 0 || typeof window === 'undefined') return;
+    if (!acc || typeof window === 'undefined') return;
 
-    const provider = new ethers.JsonRpcProvider(RPC_PROVIDER);
-    const coinflip = createCoinflip(acc);
+    const provider = new ethers.WebSocketProvider(WEBSOCKET_PROVIDER);
+    const coinflip = new ethers.Contract(CONTRACT_ADDRESS, coinflipABI, provider);
 
     if (!coinflip) {
-      console.log("Failed to create coinflip contract.")
+      console.log("Failed to create coinflip contract.");
       return;
     }
 
-    let intervalId: NodeJS.Timeout | null = null;
+    console.log(`Setting up event listeners for contract at address: ${CONTRACT_ADDRESS}`);
 
-    const startPolling = () => {
-      intervalId = setInterval(async () => {
-        const currentBlock = await provider.getBlockNumber();
-        console.log("currentBlock", currentBlock);
-        // Listen to Won and Lost events
-        const filter = {
-          address: CONTRACT_ADDRESS,
-          fromBlock: currentBlock - 10,
-          toBlock: 'latest',
-          topics: [
-            [
-              ethers.id("Won(address,uint64,uint256,uint256)"),
-              ethers.id("Lost(address,uint64,uint256,uint256)"),
-            ],
-            "0x000000000000000000000000" + acc?.address.slice(2)  // User address
-          ]
-        };
-
-        try {
-          const logs = await provider.getLogs(filter);
-          console.log("Logs:", logs);
-
-          for (const log of logs) {
-            const parsedLog = coinflip.interface.parseLog(log);
-            if (!parsedLog) {
-              console.log("NULL log:", log);
-              continue;
-            }
-            const { sequenceNumber: logSequence, finalNumber } = parsedLog.args;
-            if (logSequence === sequenceNumber) {
-              console.log("Analyse:");
-              console.log("logSequence:", logSequence);
-              console.log("finalNumber:", finalNumber);
-              console.log("log name:", parsedLog.name);
-
-              setSequenceNumber(0);
-              clearInterval(intervalId!);  // Stop polling once event is found
-
-              if (parsedLog.name === 'Lost') {
-                toast({
-                  title: "Lost!",
-                  description: `You bet on ${finalNumber > 50 ? 'TAILS' : 'HEADS'} but the result was ${finalNumber > 50 ? 'HEADS' : 'TAILS'}`,
-                  status: "warning",
-                  duration: 9000,
-                  isClosable: true,
-                });
-              } else if (parsedLog.name === 'Won') {
-                toast({
-                  title: "Won!",
-                  description: `You bet on ${finalNumber > 50 ? 'HEADS' : 'TAILS'} and the result was ${finalNumber > 50 ? 'HEADS' : 'TAILS'}`,
-                  status: "success",
-                  duration: 9000,
-                  isClosable: true,
-                });
-              } else {
-                console.log("Unknown event:", parsedLog.name);
-              }
-              setIsLoading(false);
-              break;
-            }
-          }
-        } catch (error) {
-          console.error("Error fetching logs:", error);
+    coinflip.on('Won', async (user, seqNum, finalNumber, betAmount, event) => {
+      console.log(`Won event detected for sequence number: ${seqNum}`);
+      console.log(`Current sequence number: ${sequenceNumberRef.current}`);
+      try {
+        // don't check sequence number because could face race condition
+        if (user.toLowerCase() === acc?.address.toLowerCase()) {
+          setSequenceNumber(0);
+          setIsLoading(false);
+          refetchUserBalance();
+          refetchContractBalance();
+          toast({
+            title: "Won!",
+            description: `You bet on ${finalNumber > 50 ? 'HEADS' : 'TAILS'} and the result was ${finalNumber > 50 ? 'HEADS' : 'TAILS'}`,
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
         }
-      }, 1000);  // Poll every second for responsiveness
-    };
-
-    startPolling();
+      } catch (error) {
+        console.error("Error processing Transfer event:", error);
+        setSequenceNumber(0);
+        setIsLoading(false);
+        toast({
+          title: "Error",
+          description: "An error occurred while processing the event.",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    });
+    coinflip.on('Lost', async (user, seqNum, finalNumber, betAmount, event) => {
+      console.log(`Lost event detected for sequence number: ${seqNum}`);
+      console.log(`Current sequence number: ${sequenceNumberRef.current}`);
+      try {
+        // don't check sequence number because could face race condition
+        if (user.toLowerCase() === acc?.address.toLowerCase()) {
+          setSequenceNumber(0);
+          setIsLoading(false);
+          refetchUserBalance();
+          refetchContractBalance();
+          toast({
+            title: "Lost!",
+            description: `You bet on ${finalNumber > 50 ? 'TAILS' : 'HEADS'} but the result was ${finalNumber > 50 ? 'HEADS' : 'TAILS'}`,
+            status: "warning",
+            duration: 9000,
+            isClosable: true,
+          });
+        }
+      } catch (error) {
+        console.error("Error processing Transfer event:", error);
+        setSequenceNumber(0);
+        setIsLoading(false);
+        toast({
+          title: "Error",
+          description: "An error occurred while processing the event.",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    });
 
     return () => {
-      if (intervalId !== null) {
-        clearInterval(intervalId);
-      }
+      // Clean up the event listeners
+      coinflip.removeAllListeners('Won');
+      coinflip.removeAllListeners('Lost');
+      // Close the WebSocket connection
+      provider.destroy();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [acc, sequenceNumber]); // Only run the effect if account or sequence number changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [acc]);
 
   const handleFlipClick = async () => {
     if (amountToBet === '') {
@@ -167,7 +178,7 @@ export default function Home() {
             >
               HEADS
             </Button>
-            <Button 
+            <Button
               border={coinFace == 'TAILS' ? '4px' : ''}
               borderColor={coinFace == 'TAILS' ? 'monadoffwhite' : ''}
               backgroundColor="monadpurple"
@@ -177,17 +188,36 @@ export default function Home() {
               TAILS
             </Button>
           </Flex>
-          <Text>Contract balance: {isContractBalanceLoading ? 'Loading...' : contractBalance?.displayValue}</Text>
-          <Text>Max to bet: {isContractBalanceLoading ? 'Loading...' : ethers.formatEther(contractBalance!.value / 2n)}</Text>
-          <Input 
-            placeholder="Amount to bet" 
+          <StatGroup gap={10} alignItems="center" m={4} pl={3}>
+            <Stat>
+              <StatLabel>Your balance</StatLabel>
+              <StatNumber>
+                {isUserBalanceLoading ? 'Loading...' : userBalance?.displayValue.slice(0, 6)}
+              </StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Protocol balance</StatLabel>
+              <StatNumber>
+                {isContractBalanceLoading ? 'Loading...' : contractBalance?.displayValue}
+              </StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Max to bet</StatLabel>
+              <StatNumber>
+                {isContractBalanceLoading ? 'Loading...' : ethers.formatEther(contractBalance!.value / 2n)}
+              </StatNumber>
+            </Stat>
+          </StatGroup>
+          <Input
+            placeholder="Amount to bet"
             type="number"
             onChange={(e) => setAmountToBet(e.target.value)}
+            width={"auto"}
           />
-          <Button 
-            backgroundColor="monadpurple" 
-            _hover={{ backgroundColor: "monadblue" }} 
-            onClick={handleFlipClick} 
+          <Button
+            backgroundColor="monadpurple"
+            _hover={{ backgroundColor: "monadblue" }}
+            onClick={handleFlipClick}
             isLoading={isLoading}
           >
             FLIP A COIN
